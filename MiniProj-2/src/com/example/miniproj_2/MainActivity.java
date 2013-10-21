eccc9fa80becaab45aaf2f85bf12ec884d063e1f
Catherine <cthrnpaul@gmail.com>
2013-10-20 23:23:14 -0500

    MiniProject Phase 2

---
+++ MiniProj-2/src/com/example/miniproj_2/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.miniproj_2;
+
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.common.AccountPicker;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+import android.os.Bundle;
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	static final int REQUEST_CODE_RECOVER_PLAY_SERVICES = 1001;
+	static final int REQUEST_CODE_PICK_ACCOUNT = 1002;;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	protected void onResume() {
+	  super.onResume();
+	  if (checkPlayServices() && checkUserAccount()) {
+		  Toast.makeText(this, "User logged in", 
+		          Toast.LENGTH_LONG).show();
+		      finish();
+	  }
+	}
+
+	private boolean checkPlayServices() {
+		// TODO Auto-generated method stub
+		 int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		  if (status != ConnectionResult.SUCCESS) {
+		    if (GooglePlayServicesUtil.isUserRecoverableError(status)) {
+		      showErrorDialog(status);
+		    } else {
+		      Toast.makeText(this, "This device is not supported.", 
+		          Toast.LENGTH_LONG).show();
+		      finish();
+		    }
+		    return false;
+		  }
+		  return true;
+		} 
+		 
+		void showErrorDialog(int code) {
+		  GooglePlayServicesUtil.getErrorDialog(code, this, 
+		      REQUEST_CODE_RECOVER_PLAY_SERVICES).show();
+	}
+
+	private boolean checkUserAccount() {
+		// TODO Auto-generated method stub
+		 String accountName = AccountUtils.getAccountName(this);
+		  if (accountName == null) {
+		    // Then the user was not found in the SharedPreferences. Either the
+		    // application deliberately removed the account, or the application's
+		    // data has been forcefully erased.
+		    showAccountPicker();
+		    return false;
+	}
+		  Account account = AccountUtils.getGoogleAccountByName(this, accountName);
+		  if (account == null) {
+		    // Then the account has since been removed.
+		    AccountUtils.removeAccount(this);
+		    showAccountPicker();
+		    return false;
+		  }
+		 
+		  return true;
+		}
+	private void showAccountPicker() {
+		  Intent pickAccountIntent = AccountPicker.newChooseAccountIntent(
+		      null, null, new String[] { GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE }, 
+		      true, null, null, null, null);
+		  startActivityForResult(pickAccountIntent, REQUEST_CODE_PICK_ACCOUNT);
+		}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	  switch (requestCode) {
+	    case REQUEST_CODE_RECOVER_PLAY_SERVICES:
+	      /* ... */
+	    case REQUEST_CODE_PICK_ACCOUNT:
+	      if (resultCode == RESULT_OK) {
+	        String accountName = data.getStringExtra(
+	            AccountManager.KEY_ACCOUNT_NAME);
+	        AccountUtils.setAccountName(this, accountName);
+	      } else if (resultCode == RESULT_CANCELED) {
+	        Toast.makeText(this, "This application requires a Google account.", 
+	            Toast.LENGTH_SHORT).show();
+	        finish();
+	      }
+	      return;
+	   }
+	   super.onActivityResult(requestCode, resultCode, data);
+	 }
+}
